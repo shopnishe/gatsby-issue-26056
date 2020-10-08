0449fa53c63b76409ce26190c0e2212c39f9f997
Aaron Adams <aaron@aaronadams.ca>
2020-10-08 16:36:37 +0000

    Add settings for newly private Sanity dataset

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 module.exports = {
   plugins: [
     {
       resolve: "gatsby-source-sanity",
       options: {
-        projectId: "tcytzgj5",
-        dataset: "production",
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        token: process.env.SANITY_AUTH_TOKEN,
+        overlayDrafts:
+          process.env.NODE_ENV === "development" &&
+          process.env.SANITY_AUTH_TOKEN &&
+          true,
+        watchMode:
+          process.env.NODE_ENV === "development" &&
+          process.env.SANITY_AUTH_TOKEN &&
+          true,
       },
     },
   ],
